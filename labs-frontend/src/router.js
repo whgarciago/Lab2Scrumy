957f68c1bca5efe863114797b967b8a9a45e3698
whgarciago <whgarciago@unal.edu.co>
2021-12-08 11:48:19 -0500

    Primer commit

---
+++ labs-frontend/src/router.js
@@ -0,0 +1,50 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import SignUp from "./views/SignUp";
+import Login from "./views/Login";
+import Home from "./views/Home";
+import AddRole from "./components/AddRole";
+import Courses from '@/views/Courses';
+import Roles from "@/components/Roles";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/registro",
+      name: "signup",
+      component: SignUp
+    },
+    {
+      path: "/",
+      alias: "/iniciar-sesion",
+      name: "login",
+      component: Login
+    },
+    {
+      path: '/mis-cursos',
+      name: 'courses',
+      component: Courses
+    },
+    {
+      path: "/principal",
+      name: "home",
+      component: Home,
+      children: [
+        {
+          path: "nuevo-rol",
+          name: "add-role",
+          component: AddRole
+        },
+        {
+          path: "roles",
+          name: "roles",
+          component: Roles
+        }
+      ]
+    }
+  ]
+})
---
+++ labs-frontend/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container-fluid mt-2">
+    <h1>Bienvenido al dummy</h1>
+    <div class="row">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+  import {getAuthenticationToken} from '@/dataStorage';
+
+  export default{
+    name: "Home",
+    beforeCreate( ){
+      if( !getAuthenticationToken( ) ){
+        this.$router.push( {name: 'login'} )
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
